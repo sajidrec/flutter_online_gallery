8853d9064979c789243ebe3dfaac081b7463bd6b
MD. Sajid Hossain <sajidrec@gmail.com>
2024-04-06 01:18:29 +0600

    basic file upload and show image operation can be done.

--- lib/presentation/screens/gallery_screen.dart
+++ lib/presentation/screens/gallery_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:online_gallery_app/presentation/utils/app_color_utils.dart';
 import 'package:online_gallery_app/presentation/widgets/appbar.dart';
 
 class GalleryScreen extends StatefulWidget {
@@ -9,10 +11,64 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
+  final List<String> _listOfAllImages = [];
+  bool _networkLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllItemLists();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(pageName: "Gallery"),
+      body: Visibility(
+        visible: !_networkLoading,
+        replacement: Center(
+          child: CircularProgressIndicator(
+            color: AppColorsUtil.appColor,
+          ),
+        ),
+        child: RefreshIndicator(
+          onRefresh: () => _getAllItemLists(),
+          child: (_listOfAllImages.isEmpty)
+              ? const Center(
+                  child: Text("No Item Found"),
+                )
+              : GridView.builder(
+                  itemCount: _listOfAllImages.length,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      height: 500,
+                      width: 200,
+                      child: Image.network(
+                        _listOfAllImages[index],
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                ),
+        ),
+      ),
     );
+  }
+
+  Future<void> _getAllItemLists() async {
+    _listOfAllImages.clear();
+    _networkLoading = true;
+    setState(() {});
+    final Reference storageRef = FirebaseStorage.instance.ref();
+    final ListResult listResult = await storageRef.listAll();
+    for (Reference item in listResult.items) {
+      _listOfAllImages.add(await item.getDownloadURL());
+      print("${await item.getDownloadURL()} <-> Image link");
+    }
+    _networkLoading = false;
+    setState(() {});
   }
 }
